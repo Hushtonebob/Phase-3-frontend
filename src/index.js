5cd21dcf3327ca98dbfefc5560b98fb3ef60fefb
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-17 20:06:18 -0700

    changed the directory order

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from '/Users/bobbydeluna/Desktop/Phase-3-frontend/src/Components/App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
